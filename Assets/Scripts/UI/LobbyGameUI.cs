9cac33053bfe20b77f12dd0420512ff984964c9c
rekaya <2unryoung@gmail.com>
2023-02-16 15:45:59 +0900

    로비 UI, 설정창 UI, UI스크립트, 방치형보상수령창 UI제작

---
+++ Assets/Scripts/UI/LobbyGameUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbyGameUI : MonoBehaviour
+{
+    public void OnButtonGotoCafeScene()
+    {
+        SceneManager.LoadScene("Cafe");
+    }
+    public void OnButtonGotoCharacterScene()
+    {
+        SceneManager.LoadScene("Character");
+    }
+
+    public void OnButtonGotoShopScene()
+    {
+        SceneManager.LoadScene("Shop");
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
